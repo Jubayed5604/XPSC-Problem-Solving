5c9ca32c22ce524b8dda595364939a1f05060185
HM Jubayed <161754242+Jubayed5604@users.noreply.github.com>
2024-10-29 23:11:43 +0600

    Add files via upload
    DAY 3

---
+++ Day 3/B_Heat_Wave.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+    
+    int x, y; cin >> x >> y; 
+    if(x<y)cout << "YES" << endl;
+    else cout << "NO" << endl;
+return 0;
+}
