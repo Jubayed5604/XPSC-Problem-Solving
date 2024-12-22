50a9c1de2d38694f472d5afbfb55f39ffeff7f86
HM Jubayed <161754242+Jubayed5604@users.noreply.github.com>
2024-12-22 23:37:45 +0600

    Week 9

---
+++ B_Journey.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define fast_io() ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+#ifdef ONLINE_JUDGE
+#define debug(...) 41
+#else
+#include "E:\debug.h"
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+string toBinary(int num, int n) {
+    string binary = "";
+    while (num > 0) {
+        binary = char('0' + (num % 2)) + binary;
+        num /= 2;
+    }
+    while(binary.size() < n)binary = '0' + binary;
+    return binary;
+}
+
+void solve() {
+    ll n, a, b, c; cin >> n >> a >> b >> c;
+    ll total = a + b + c;
+    int ans = n / total;
+    //cout << ans << endl;
+    int cc = 0;
+    if(ans * total < n){
+        n -= a; 
+        cc++;
+    }
+    if(ans * total < n){
+        n -= b;
+        cc++;
+    }
+    if(ans * total < n)cc++;
+
+    cout << ans * 3 + cc << endl;
+}
+
+int main() {
+    fast_io();
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
