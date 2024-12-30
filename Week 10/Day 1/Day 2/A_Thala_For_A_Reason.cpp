bd155b76916fc46b60348f8da0fe497479956988
HM Jubayed <161754242+Jubayed5604@users.noreply.github.com>
2024-12-30 23:31:49 +0600

    Add files via upload

---
+++ Week 10/Day 1/Day 2/A_Thala_For_A_Reason.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define fast_io() ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+#ifdef ONLINE_JUDGE
+#define debug(...) 41
+#else
+#include "E:\debug.h"
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+string toBinary(int num, int n) {
+    string binary = "";
+    while (num > 0) {
+        binary = char('0' + (num % 2)) + binary;
+        num /= 2;
+    }
+    while(binary.size() < n)binary = '0' + binary;
+    return binary;
+}
+
+void solve() {
+    int n; cin >> n;
+    if(n==7)cout << "THALA" << endl;
+    else cout << "SADGE" << endl;
+}
+
+int main() {
+    fast_io();
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
