f3c02d96ff5cc7ec50bb02b29e68e536039d4a0c
HM Jubayed <161754242+Jubayed5604@users.noreply.github.com>
2025-01-12 22:29:31 +0600

    Add files via upload

---
+++ Week 11/Day 3/Day 7/A_Question_Marks.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define fast_io() ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+#ifdef ONLINE_JUDGE
+#define debug(...) 41
+#else
+#include "D:\debug.h"
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+string toBinary(int num, int n) {
+    string binary = "";
+    while (num > 0) {
+        binary = char('0' + (num % 2)) + binary;
+        num /= 2;
+    }
+    while(binary.size() < n)binary = '0' + binary;
+    return binary;
+}
+
+void solve() {
+    int n; cin >> n;
+    string s; cin >> s;
+    map<char,int>mp;
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] != '?')
+        mp[s[i]]++;
+    }
+    int ans = 0;
+    for(auto i: mp){
+        if(i.second > n){
+            ans += n;
+        }
+        else ans += i.second;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    fast_io();
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
