ce6d1d7212011dea89cc9076a24829edaf1d43cf
HM Jubayed <161754242+Jubayed5604@users.noreply.github.com>
2025-02-02 23:13:58 +0600

    Add files via upload

---
+++ Week 14/Day 1/Day 5/A_Buy_a_Shovel.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define fast_io() ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+#ifdef ONLINE_JUDGE
+#define debug(...) 41
+#else
+#include "D:\debug.h"
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+string toBinary(int num, int n) {
+    string binary = "";
+    while (num > 0) {
+        binary = char('0' + (num % 2)) + binary;
+        num /= 2;
+    }
+    while(binary.size() < n)binary = '0' + binary;
+    return binary;
+}
+
+void solve() {
+    int n, k; cin >> n >> k;
+    int ans = 0;
+    for(int i = 1; i <= 1000; i++){
+        if(((n*i) % 10)== 0 or  (n*i) % 10 == k){
+            ans = i;
+            break;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    fast_io();
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
---
+++ Week 14/Day 1/Day 5/A_Milya_and_Two_Arrays.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define fast_io() ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+#ifdef ONLINE_JUDGE
+#define debug(...) 41
+#else
+#include "D:\debug.h"
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+string toBinary(int num, int n) {
+    string binary = "";
+    while (num > 0) {
+        binary = char('0' + (num % 2)) + binary;
+        num /= 2;
+    }
+    while(binary.size() < n)binary = '0' + binary;
+    return binary;
+}
+
+void solve() {
+    int n; cin >> n;
+    vi v1(n),v2(n);
+    set<int>s1,s2;
+    for (int i = 0; i < n; i++) {
+        cin >> v1[i];
+        s1.insert(v1[i]);
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> v2[i];
+        s2.insert(v2[i]);
+    }
+
+    if(s1.size() + s2.size() >= 4)cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
+
+int main() {
+    fast_io();
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
