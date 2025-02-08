67e29a249fcafb841bd518ea6850eef24c10de9f
HM Jubayed <161754242+Jubayed5604@users.noreply.github.com>
2025-02-08 23:17:03 +0600

    Add files via upload

---
+++ Week 15/Day 1/Day 2/A_Dislike_of_Threes.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define fast_io() ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+#ifdef ONLINE_JUDGE
+#define debug(...) 41
+#else
+#include "D:\debug.h"
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+string toBinary(int num, int n) {
+    string binary = "";
+    while (num > 0) {
+        binary = char('0' + (num % 2)) + binary;
+        num /= 2;
+    }
+    while(binary.size() < n)binary = '0' + binary;
+    return binary;
+}
+
+void solve() {
+    vi v;
+    v.push_back(0);
+    for(int i = 1; i <= 1666; i++){
+        if(i % 3 != 0 and i % 10 != 3){
+            v.push_back(i);
+        }
+    }
+    int n; cin >> n;
+    cout << v[n] << endl;
+}
+
+int main() {
+    fast_io();
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
