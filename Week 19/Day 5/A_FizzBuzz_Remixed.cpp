602f809154924f2ab357f2a9ec3ac28bb9a9f2ae
HM Jubayed <161754242+Jubayed5604@users.noreply.github.com>
2025-03-13 22:46:57 +0600

    Add files via upload

---
+++ Week 19/Day 5/A_FizzBuzz_Remixed.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define fast_io() ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+#ifdef ONLINE_JUDGE
+#define debug(...) 41
+#else
+#include "D:\debug.h"
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+string toBinary(int num, int n) {
+    string binary = "";
+    while (num > 0) {
+        binary = char('0' + (num % 2)) + binary;
+        num /= 2;
+    }
+    while(binary.size() < n)binary = '0' + binary;
+    return binary;
+}
+
+void solve() {
+    ll n; cin >> n;
+    ll cnt = n / 15;
+    ll rem = n % 15;
+    ll ans = cnt * 3;
+    if(rem >= 3){
+        ans += 3;
+    }
+    else ans += rem + 1;
+    cout << ans << "\n";
+}
+
+
+int main() {
+    fast_io();
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
